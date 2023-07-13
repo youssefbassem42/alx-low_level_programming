4d6f672bd80ba6ffd1eb7f90bedd5434ff0a17e4
youssefbassem42 <youssefbassem42@gmail.com>
2023-07-13 06:02:16 -0700

    5th

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *	A main function -Entry point
+ *	A C program that prints a word with function puts
+ *	return : Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
