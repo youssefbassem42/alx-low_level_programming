abcee3cfd5b2399d2e7b11eadee228d423bfe6da
youssefbassem42 <youssefbassem42@gmail.com>
2023-07-13 06:32:40 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/*
- *	Description: Main - Entry Point function
- *	Description: A C program prints a word with printf function
- *	Return: Always 0 (Seuccess)
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program prints a word with printf function
+ *
+ * Return: Always 0 (Seuccess)
 */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
