531c9941a20c8228f7ef86bd7b50b0303d00d7bd
youssefbassem42 <youssefbassem42@gmail.com>
2023-07-13 06:22:26 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *	Description: Main - Entry Point function
+ *	Description: A C program prints a word with printf function
+ *	Return: Always 0 (Seuccess)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
