5bd37a147bd2df227990f0a518b5003e133b43c0
youssefbassem42 <youssefbassem42@gmail.com>
2023-08-31 14:15:25 -0700

    logic

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b)
 	{
-		if (*b != '0' || *b != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 		number = 2 * number + (*b++ - '0');
 	}
