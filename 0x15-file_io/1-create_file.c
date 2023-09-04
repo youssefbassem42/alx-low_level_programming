0751ea0874547369b5cde3b1b760bd90d3baada8
youssefbassem42 <youssefbassem42@gmail.com>
2023-09-04 13:50:13 -0700

    fix length

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[length];)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 
 	file_dir = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(file_dir, text_content, len);
+	w = write(file_dir, text_content, length);
 
 	if (file_dir == -1 || w == -1)
 		return (-1);
