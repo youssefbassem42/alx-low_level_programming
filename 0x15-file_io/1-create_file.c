adcc94cebdd9fb6fdf01e23b07fe9f3662362e7d
yousefbassem42 <yousefbassem42@gmail.com>
2023-10-02 15:05:41 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
-			length++;
+		length++;
 	}
-	
+
 	filed = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(filed, text_content, length);
 
@@ -26,5 +26,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(filed);
-	return(1);
+	return (1);
 }
