0bf89ae23251f6b86c08c093b381961536751847
yousefbassem42 <yousefbassem42@gmail.com>
2023-10-02 15:02:13 -0700

    logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
